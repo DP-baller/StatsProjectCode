eb80e3fa28e9125fe376c38bb9cbe2030791f03c
Dan Pop <danpop32359@gmail.com>
2023-04-14 22:06:10 -0400

    Added Seaborn scatterplot & jointplot

--- CaffeineGroupCode.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-
-y = pd.read_csv('SoftDrink.csv')
-
-plt.pie(y['Brand Purchased'].value_counts(), labels =y['Brand Purchased'].unique(), autopct='%1.1f%%')
-plt.show() 
